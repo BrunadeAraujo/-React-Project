e3bf7af26fac0ca705054771379cad679405c746
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-07-28 10:58:35 -0300

    form/botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,20 @@
 import './Formulario.css';
 import CampoTexto  from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
+
+    const times =[
+        'Programação',
+        'Front End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
      <section className="formulario">
          <form>
@@ -9,10 +22,11 @@ const Formulario = () => {
             <CampoTexto label="Nome" placeholder="Digite Seu Nome"/>
             <CampoTexto label="Cargo"placeholder="Digite Seu Cargo"/>
             <CampoTexto label="imagem"placeholder="Digite o endereço da imagem"/>
+            <ListaSuspensa label ="Time" itens={times}/>
+            <Botao texto="Criar Card"/>
         </form>
      </section>
     )
-
 } 
 
 export default Formulario;
