12158eb2b8daa6cabc74b3845ee3a631e3f62662
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-08-03 18:29:32 -0300

    campoTexto

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,8 @@ const ListaSuspensa = (props) => {
     return(
       <div className='lista-suspensa'> 
          <label>{props.label}</label>
-         <select>
+         
+         <select required={props.required}>
             {props.itens.map(item => {
               return <option key={item}>{item}</option>
            })}
